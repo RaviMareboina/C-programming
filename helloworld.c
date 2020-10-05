9a19431c8205d4d17576dcadcdb5c04f96bdc9af
Mareboina Ravi <51162616+MareboinaRavi@users.noreply.github.com>
2020-10-05 10:45:52 +0530

    creating new file
    optional;

---
+++ helloworld.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main(){
+	//code-1
+//	int m=100;
+//	{
+//		int m;
+//		for(m=0;m<25;m++);
+//	 } 
+//	printf("%d",m);
+
+//code - 2
+
+
+//int y=128;
+//const int x=y;
+//printf("%d",y+1);
+
+//code - 3
+//int i=0;
+//int c=1;
+//do{
+//	i++;   
+//	//int c=1;
+//	printf("Ravi \n %d",c);
+//	c++;
+//}while(i < 2020);
+
+// code -4
+//int x;
+//for(x=-1;x<=10;x++){
+//	if(x<5)continue;
+//	else break;
+//	printf("abc \n");
+//}
+
+//code -5
+
+int x;
+x = scanf("%d %d ",&x, &x);
+printf("%d\n",x);
+	return 0; 
+}
